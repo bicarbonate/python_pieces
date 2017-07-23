837d065deaedb378b7f1405a7a0c7996f6cc82f3
bicarbonate <invalid.path@gmail.com>
2017-07-23 12:20:44 -0600

    new piece for a manual else if

--- paramiko_snippet.py
+++ paramiko_snippet.py
@@ -1,31 +1,19 @@
-import sys, paramiko
+import sys, paramiko, getpass
 
-#Requests user input for a target IP address
-#print = "Enter the target IP"
-#hostname = input('Enter the target IP address')
 
-#Sets env variables
-user = 'skeer'
-password = 'Oplsai386'
-hosts = 'sputnik'
+user = raw_input("Username:") #Prompts for username
+passwd = getpass.getpass("Password for " + user + ":") #Prompts for password
+name = raw_input("Target Hostname:") #Prompt for target hostname
 
-print( #Greeting Messges.
-    "\n"
-    "1 Enter your Username"
-    "\n2 Enter your password"
-    "\n3 Enter the hostname")
 
 
 #Calling the paramiko ssh function
 ssh = paramiko.SSHClient()
 print('calling paramiko')
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-conn = ssh.connect("hosts", username="user", password="password")
+conn = ssh.connect("name", username="user", password="passwd")
 print('trying to connect')
-cmd = 'uptime'
-print('')
-print(cmd)
-(stdin, stdout, stderr) = ssh.exec_command(cmd)
+
 z = stdout.read()
 print(z)
 #ssh.close()
